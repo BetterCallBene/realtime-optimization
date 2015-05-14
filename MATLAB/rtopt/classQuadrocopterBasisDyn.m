d30b7c45c3e1e88b1b560ca125bc13878951fe5f
Philipp Fröhlich <philipp-froehlich@hotmail.com>
2015-05-14 15:45:41 +0200

    Debugging von testdotD,testdotDD ;  Refactoring um Code objektorientierter zu machen

--- MATLAB/rtopt/classQuadrocopterBasisDyn.m
+++ MATLAB/rtopt/classQuadrocopterBasisDyn.m
@@ -1,4 +1,4 @@
-classdef classQuadrocopterBasisDyn < handle & classTestEnv 
+classdef(Abstract) classQuadrocopterBasisDyn <  classDyn & classTestEnv 
     %CLASSQUADROCOPTERBASISDYN Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -6,37 +6,20 @@ classdef classQuadrocopterBasisDyn < handle & classTestEnv
         F;
         J;
         H;
-        
-        state; % the state matrix [r, q, v, w] in R^(13xn) for all time instances
-        contr; % the control matrix [u1, u2, u3, u4] in R^(4xn) for all time instances
     end
     
     properties(Constant, GetAccess=private)
+        %TODO: they come from the model -> model.getI, model.getI_M ...
         I = [0.0093886, 0.0093886, 0.018406];
-        % Trägheitsmoment gesamt in kg m^2
+        % Trï¿½gheitsmoment gesamt in kg m^2
         m   = 1.022;                                % Gesamtgewicht des Quadrokopters in kg
-        I_M = 0.0001;     %ToDo                              % Trägheitsmoment der Motoren/Rotoren in kg m^2
+        I_M = 0.0001;     %ToDo                              % Trï¿½gheitsmoment der Motoren/Rotoren in kg m^2
         kT  = 1.5e-07;                              % N/(RPM^2)
         kQ  = 3e-09;                                % N*m/(RPM)^2
         d   = 0.22;                                 % Abstand in m
         g   = 9.81;
     end
-    
-    properties(...
-        Dependent, ...
-        GetAccess = public ...
-    )
-        n_state_contr;
-    end
-
-    properties(...
-            Constant, ...
-            GetAccess = public ...
-            )
-        n_var = 13;        % Count of Variables
-        n_contr = 4;       % Count of Controls
-    end
-    
+        
     properties(GetAccess=private, SetAccess = protected)
          n_int;
          isEmptyF;
@@ -51,15 +34,7 @@ classdef classQuadrocopterBasisDyn < handle & classTestEnv
     end
     
     methods
-        function cQ = classQuadrocopterBasisDyn(vargin)
-            %cQ@classModell();
-        end
-
-        
-        function ret = get.n_state_contr(obj)
-            ret = obj.n_contr + obj.n_var;
-        end
-        
+       
         function emptyResults(obj)
             
             %emptyResults@classModell(obj);
@@ -73,33 +48,6 @@ classdef classQuadrocopterBasisDyn < handle & classTestEnv
             obj.isEmptyH = true;
         end
 
-        function set.state(cq, state)
-            
-            if size(state, 1) ~= 13
-                error('Größe der State Matrix ist falsch. Erwartete Größe 13xn');
-            end
-            n = size(state, 2);
-            cq.set_interval(n);
-            
-            for i=1:n
-                q = state(4:7, i);
-                state(4:7, i) = q./norm(q);
-            end
-            
-            cq.state = state;
-            cq.emptyResults();
-        end
-        
-        function set.contr(cq, cntrl)
-            
-            if size(cntrl, 1) ~= 4
-                error('Größe der State Matrix ist falsch. Erwartete Größe 4xn');
-            end
-            cq.set_interval(size(cntrl, 2));
-            cq.contr = cntrl;
-            cq.emptyResults();
-        end
-        
         function res = get.F(obj)
             if obj.isEmptyF 
                 
--- MATLAB/rtopt/classQuadrocopterDyn.m
+++ MATLAB/rtopt/classQuadrocopterDyn.m
@@ -1,21 +1,6 @@
 classdef classQuadrocopterDyn < classQuadrocopterBasisDyn & classTestEnv
-% classDyn providing the right hand side of the ODE
-%   Reformulation of the second-order ODE as a first-order ODE
-
-    properties
-        %contr; % the control vector combining the two joint torque values for all time instances
-        robot; % handle for a classRobot element providing the mass matrix and the coriolis terms
-    end
-    
-    properties(SetAccess = private, GetAccess = private)
-    end
-    
-    properties(Dependent, GetAccess= public)
-        %n_var;
-        %n_contr;
-    end
-    
-    
+    % classDyn providing the right hand side of the ODE
+    %   Reformulation of the second-order ODE as a first-order ODE
     
     methods
         %constructor
@@ -30,26 +15,26 @@ classdef classQuadrocopterDyn < classQuadrocopterBasisDyn & classTestEnv
                     error('wrong class type for robot');
                 end
             else
-                error('wrong number of inputs');     
+                error('wrong number of inputs');
             end
         end
         
-%         function ret =  get.n_var(obj)
-%             if (~isempty(obj.robot))
-%                 ret = obj.robot.n_var;
-%             end
-%         end
-%         
-%         function ret = get.n_contr(obj)
-%             if (~isempty(obj.robot))
-%                 ret = obj.robot.n_contr;
-%             end
-%         end
+        %         function ret =  get.n_var(obj)
+        %             if (~isempty(obj.robot))
+        %                 ret = obj.robot.n_var;
+        %             end
+        %         end
+        %
+        %         function ret = get.n_contr(obj)
+        %             if (~isempty(obj.robot))
+        %                 ret = obj.robot.n_contr;
+        %             end
+        %         end
         
         %set methods
         %function set.state(obj,state)
-            % set the current values for the states at all time instances
-            % and pass it on the robot
+        % set the current values for the states at all time instances
+        % and pass it on the robot
         %    obj.state = state;
         %    if (~isempty(obj.robot))
         %        obj.robot.state = state;
@@ -58,8 +43,8 @@ classdef classQuadrocopterDyn < classQuadrocopterBasisDyn & classTestEnv
         
         %set methods
         %function set.contr(obj,contr)
-            % set the current values for the states at all time instances
-            % and pass it on the robot
+        % set the current values for the states at all time instances
+        % and pass it on the robot
         %    obj.contr = contr;
         %    if (~isempty(obj.robot))
         %        obj.robot.contr = contr;
@@ -67,7 +52,7 @@ classdef classQuadrocopterDyn < classQuadrocopterBasisDyn & classTestEnv
         %end
         
         % other functions
-        % TODO: Mapleskript einpflegen, Dynamik 
+        % TODO: Mapleskript einpflegen, Dynamik
         function res = dot(obj,ind) % Rechte Seite der ODE aus (Quadrocoptermodell.pdf: (1.46))
             % compute the right hand side of the ode for a given time
             % instance ind
@@ -81,8 +66,8 @@ classdef classQuadrocopterDyn < classQuadrocopterBasisDyn & classTestEnv
         % Testen
         
         function res = dotD(obj,ind)
-            % compute the Jacobian of the right hand side of the ode for 
-            % a given time instance ind            
+            % compute the Jacobian of the right hand side of the ode for
+            % a given time instance ind
             if ((size(obj.contr,2)==size(obj.state,2))&&(ind <= size(obj.contr,2)))
                 
                 J_ = obj.J;
@@ -96,8 +81,8 @@ classdef classQuadrocopterDyn < classQuadrocopterBasisDyn & classTestEnv
         end
         
         function res = dotDD(obj,ind)
-            % compute the Hessian of the right hand side of the ode for 
-            % a given time instance ind                
+            % compute the Hessian of the right hand side of the ode for
+            % a given time instance ind
             if ((size(obj.contr,2)==size(obj.state,2))&&(ind <= size(obj.contr,2)))
                 res = cell(1, 13);
                 H_ = obj.H;
@@ -107,56 +92,7 @@ classdef classQuadrocopterDyn < classQuadrocopterBasisDyn & classTestEnv
                     end
                     res{H_(i, 1)}(H_(i, 2), H_(i, 3)) = H_(i, ind + 3);
                 end
-            end    
-        end  
-    end
-    
-    methods(Test)
-%         function testdot(obj)
-%             n_int_ = 50;
-%             ind = 50;
-%             
-%             n_var_ = obj.n_var;
-%             n_contr_ = obj.n_contr;
-%             
-%             obj.state = rand(n_var_, n_int_+1);
-%             obj.contr = rand(n_contr_, n_int_+1);
-%             
-%             val = obj.dot(ind);
-%             
-%             obj.verifySize(val, [13, 1]);
-%        end
-        
-        function testdotD(obj)
-            n_int_ = 50;
-            ind = 2;
-            
-            n_var_ = obj.n_var;
-            n_contr_ = obj.n_contr;
-            
-            obj.state = rand(n_var_, n_int_+1);
-            obj.contr = rand(n_contr_, n_int_+1);
-            
-            val = obj.dotD(ind);
-            
-            obj.verifySize(val, [13, 17]);
-        end
-        
-        function testdotDDs(obj)
-            n_int_ = 50;
-            ind = 10;
-            
-            n_var_ = obj.n_var;
-            n_contr_ = obj.n_contr;
-            
-            obj.state = rand(n_var_, n_int_+1);
-            obj.contr = rand(n_contr_, n_int_+1);
-            
-            val = obj.dotDD(ind);
-            
-            obj.verifySize(val, [1, 13]);
+            end
         end
     end
-    
-    
 end
