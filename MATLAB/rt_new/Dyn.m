ce62dda0244d742090694ab32f5835f2aa5699d6
Philipp Fröhlich <philipp-froehlich@hotmail.com>
2015-05-15 19:41:33 +0200

    Implementierung von numDiff in TestEnv, allerdings noch NICHT getestet

--- MATLAB/rt_new/Dyn.m
+++ MATLAB/rt_new/Dyn.m
@@ -19,7 +19,7 @@ classdef(Abstract) Dyn < handle  & classTestEnv
         
     end
     
-     methods(Test)
+    methods(Test)
         
         function testdotD(obj)
             %TODO: Debug do we have to init timemesh?
@@ -35,30 +35,33 @@ classdef(Abstract) Dyn < handle  & classTestEnv
             
             %Test at time step 1
             %TODO: Decide, if we also want to loop over time
-            for j=1:17
-                
-                %plus epsilon shift
-                stco_p = stco;
-                stco_p(j) = stco_p(j) + eps;
-                obj.state=stco_p(1:13);
-                obj.contr=stco_p(14:17);
-                dot_p = obj.dot(1);
-                
-                %minus epsilon shift
-                stco_n = stco;
-                stco_n(j) = stco_n(j) - eps;
-                obj.state=stco_n(1:13);
-                obj.contr=stco_n(14:17);
-                dot_n = obj.dot(1);
-                
-                %central difference
-                dotD_xj = (dot_p - dot_n)/(2*eps);
-                num_dotD(:,j) = dotD_xj;
-                
-                obj.state=stco(1:13);
-                obj.contr=stco(14:17);
-                
-            end
+%             for j=1:17
+%                 
+%                 %plus epsilon shift
+%                 stco_p = stco;
+%                 stco_p(j) = stco_p(j) + eps;
+%                 obj.state=stco_p(1:13);
+%                 obj.contr=stco_p(14:17);
+%                 dot_p = obj.dot(1);
+%                 
+%                 %minus epsilon shift
+%                 stco_n = stco;
+%                 stco_n(j) = stco_n(j) - eps;
+%                 obj.state=stco_n(1:13);
+%                 obj.contr=stco_n(14:17);
+%                 dot_n = obj.dot(1);
+%                 
+%                 %central difference
+%                 dotD_xj = (dot_p - dot_n)/(2*eps);
+%                 num_dotD(:,j) = dotD_xj;
+%                 
+%                 obj.state=stco(1:13);
+%                 obj.contr=stco(14:17);
+%                 
+%             end
+            
+            func = @() obj.dot(time_step);
+            numDiff = obj.numdiffDnD(func);
             
             %Calculate analytic solution
             ana_dotD = obj.dotD(obj,1);
@@ -72,9 +75,9 @@ classdef(Abstract) Dyn < handle  & classTestEnv
         end
         
         function testdotDD(obj)
-            %TODO: Debug do we have to init timemesh? Is the data structure
-            %correct?
+            %TODO: Debug do we have to init timemesh? 
             %Initialize state with random values
+            time_step = 1;
             obj.state=rand(13,1);
             obj.contr=rand(4,1);
             
@@ -82,36 +85,65 @@ classdef(Abstract) Dyn < handle  & classTestEnv
             
             eps = 1e-6;
             
-            num_dotDD = zeros(17,13,17);
+            num_dotDD = zeros(13,17,17);
             
             %TODO: Decide if we also want to loop over time
-            for j = 1:17
-                %plus epsilon shift
-                stco_p = stco;
-                stco_p(j) = stco_p(j) + eps;
-                obj.state=stco_p(1:13);
-                obj.contr=stco_p(14:17);
-                dotD_p = obj.dotD(1);
-                
-                %minus epsilon shift
-                stco_n = stco;
-                stco_n(j) = stco_n(j) - eps;
-                obj.state=stco_n(1:13);
-                obj.contr=stco_n(14:17);
-                dotD_n = obj.dotD(1);
-                
-                %central difference
-                dotDD_xj = (dotD_p - dotD_n)/(2*eps);
-                num_dotDD(j,:,:) = dotDD_xj;
-                
-                obj.state=stco(1:13);
-                obj.contr=stco(14:17);
-            end
+%             for j = 1:n
+%                 %plus epsilon shift
+%                 stco_p = stco;
+%                 stco_p(j) = stco_p(j) + eps;
+%                 obj.state=stco_p(1:13);
+%                 obj.contr=stco_p(14:17);
+%                 dotD_p = obj.dotD(1);
+%                 
+%                 %minus epsilon shift
+%                 stco_n = stco;
+%                 stco_n(j) = stco_n(j) - eps;
+%                 obj.state=stco_n(1:13);
+%                 obj.contr=stco_n(14:17);
+%                 dotD_n = obj.dotD(1);
+%                 
+%                               
+%                 %central difference
+%                 dotDD_xj = (dotD_p - dotD_n)/(2*eps);
+%                 num_dotDD(:,j,:) = dotDD_xj;
+%                 
+%                 obj.state=stco(1:13);
+%                 obj.contr=stco(14:17);
+%             end
+            
+            func = @() obj.dotD(time_step);
+            numDiff = obj.numdiffDnxnD(func);
+            
             ana_dotDD = obj.dotDD(obj,1);
             for j = 1:length(num_dotDD)
-                %TODO: Adapt data formats
                 obj.assertLessThan(max(abs(ana_dotDD{j} - num_dotDD(j,:,:))),obj.tol);
             end
         end
+        
+        function  [vec_old, n,m] = setup(obj,func)
+            vec_old = [obj.state ; obj.contr];
+            n = length(vec_old);
+            m = length(func());
+        end
+        
+        %Overwrite from TestEnv as functions depend on state and contr
+        function func_p = plusEpsShift(obj,i,vec_old,func)
+            vec_p = vec_old;
+            vec_p(i) = vec_p(i) + obj.eps;
+            obj.state = vec_p(1:13,:);
+            obj.contr = vec_p(14:17,:);
+            func_p = func();
+        end
+        
+        %Overwrite from TestEnv as functions depend on state and contr
+        function func_n = minusEpsShift(obj,vec_old,func)
+            vec_n = vec_old;
+            vec_n(i) = vec_n(i) - obj.eps;
+            obj.state = vec_n(1:13,:);
+            obj.contr = vec_n(14:17,:);
+            func_n = func();
+        end
+        
     end
 end
--- MATLAB/rt_new/TestEnv.m
+++ MATLAB/rt_new/TestEnv.m
@@ -7,6 +7,73 @@ classdef TestEnv < matlab.unittest.TestCase
     end
     
     methods
+        
+        %TODO: debug
+        function numDiff = numDiff1D(obj,func)
+            % NUMDIFF1D This method calculates numerically the derivative numDiff
+            % of func, when func only depends on obj.vec and has 1 dim output
+            [vec_old, n,m] = obj.setup(g_obj);
+            numDiff = zeros(n,1);
+            for i=1:n
+                func_p = obj.plusEpsShift(i,vec_old,func);
+                func_n = obj.minusEpsShift(i,vec_old,func);
+                
+                %Central difference
+                numDiff(i) = (func_p - func_n)/2/obj.eps;
+            end
+        end
+        
+        %TODO: debug
+        function numDiff = numDiffDnD(obj, func)
+            % NUMDIFFDND This method calculates numerically the derivative
+            % of func, when func only depends on obj.vec and has m dim output
+            [vec_old, n,m] = obj.setup(g_obj);
+            numDiff = zeros(m,n);
+            for i=1:n
+                func_p = obj.plusEpsShift(i,vec_old,func);
+                func_n = obj.minusEpsShift(i,vec_old,func);
+                
+                %Central difference
+                numDiff(:,i) = (func_p - func_n)/2/obj.eps;
+            end
+        end
+        
+        %TODO: debug
+        function numDiff = numdiffDnxnD(obj,func)
+            % NUMDIFFDNXND This method calculates numerically the
+            % derivative of func, when func only depends on obj.vec and has
+            % n times n dimensional output
+            [vec_old, n] = obj.setup(g_obj);
+            
+            numDiff = zeros(m,n,n);
+            for i=1:n
+                func_p = obj.plusEpsShift(i,vec_old,func);
+                func_n = obj.minusEpsShift(i,vec_old,func);
+                
+                %Central difference
+                numDiff(:,i,:) = (func_p - func_n )/2/obj.eps; 
+            end
+        end
+        
+        function [vec_old, n,m] = setup(obj,g_obj,func)
+            vec_old = g_obj.vec;
+            n = length(vec_old);
+            m = length(func());
+        end
+        
+        function func_p = plusEpsShift(obj,i,vec_old,func)
+            vec_p = vec_old;
+            vec_p(i) = vec_p(i) + obj.eps;
+            obj.vec = vec_p;
+            func_p = func();
+        end
+        
+        function func_n = minusEpsShift(obj,vec_old,func)
+            vec_n = vec_old;
+            vec_n(i) = vec_n(i) - obj.eps;
+            obj.vec = vec_n;
+            func_n = func();
+        end
     end
     
 end
