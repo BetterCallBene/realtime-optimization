fee56b2064064abc31e4bbec301eff3cc5032e9e
Benedikt König <Bene@Benedikts-Air.fritz.box>
2015-05-18 22:44:21 +0200

    Traegheitsmoment angepasst

--- MATLAB/rtopt/Quadrocopter.m
+++ MATLAB/rtopt/Quadrocopter.m
@@ -9,21 +9,41 @@ classdef Quadrocopter < Model
         I = [0.0093886, 0.0093886, 0.018406];
         % Tr�gheitsmoment gesamt in kg m^2
         m   = 1.022;                                % Gesamtgewicht des Quadrokopters in kg
-        I_M = 0.0001;     %ToDo                              % Tr�gheitsmoment der Motoren/Rotoren in kg m^2
+        %I_M = 0.0001;     %ToDo                              % Tr�gheitsmoment der Motoren/Rotoren in kg m^2
         kT  = 1.5e-07;                              % N/(RPM^2)
         kQ  = 3e-09;                                % N*m/(RPM)^2
         d   = 0.22;                                 % Abstand in m
-        
+        motor_m = 0.075;                            % Masse Motor
+        motor_r = 0.015;                            % Motor R 1.5 cm
     end
+    
+    %
+    % Jm (Motor Rotation Inertia for Rotating Component only)
+    % Mass of rotating component is 52.7% of the total mass of the motor + prop
+    %mRC = (motor_m)*(0.527);
+    %Jm = ((mRC)*(motor_r)^2)/2; % Jm = mr^2/2
+    %
+    
+    
 
 	properties(Dependent)
 		n_var;
         I_1;
+        I_M;
     end
     
     methods
         function res = Quadrocopter(varign)
             
+        end
+        % Jm (Motor Rotation Inertia for Rotating Component only)
+        % Mass of rotating component is 52.7% of the total mass of the motor + prop
+        %mRC = (motor_m)*(0.527);
+        %Jm = ((mRC)*(motor_r)^2)/2; % Jm = mr^2/2
+        %
+        function IM = get.I_M(cq)
+            mRC = (cq.motor_m)*(0.527);
+            IM = ((mRC)*(cq.motor_r)^2)/2;
         end
         function ret = get.I_1(cq)
             ret = [
