6c7f8da1528255c7475ac75842331ff2482a42a0
SimZek <simon.kick@gmx.de>
2015-06-09 16:11:20 +0200

    getLD befüllt

--- MATLAB/rtopt/Realtime/getLD.m
+++ MATLAB/rtopt/Realtime/getLD.m
@@ -6,10 +6,16 @@ function [ J ] = getLD( cCost, cConst, i )
 
 
 
-% Initialize H
+% Initialize J
+
+constr = cConst.get_eq_con_at_t(i);
+cost = cCost.get_costD;
+cost = cost( (i-1)*(cConstr.dyn.robot.n_state+cConstr.dyn.robot.n_contr) + 1: i*(cConstr.dyn.robot.n_state+cConstr.dyn.robot.n_contr));
 
 
 % Fill it up
+
+J = [ cost ; constr( 2*cConstr.dyn.robot.n_state +1 : end) ; constr( cConstr.dyn.robot.n_state +1 :cConstr.dyn.robot.n_state) ];
 
 end
 
