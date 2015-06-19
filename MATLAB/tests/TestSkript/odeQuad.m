766d7f7c09e4ed4e9a60e1699418e925400aefdd
Benedikt König <Bene@Benedikts-Air.fritz.box>
2015-06-19 17:16:42 +0200

    Jacobimatrix hinzugefuegt

--- MATLAB/tests/TestSkript/odeQuad.m
+++ MATLAB/tests/TestSkript/odeQuad.m
@@ -6,7 +6,7 @@ tspan = [0.025 0.05];
 val = rand(13, 1);
 u = rand(4, 1);
 
-opts = odeset('RelTol',1e-5,'AbsTol',1e-4);
+opts = odeset('RelTol',1e-4,'AbsTol',1e-5, 'Jacobian',@Jac);
 
 val(4:7) = val(4:7)./norm(val(4:7));
 
@@ -14,13 +14,20 @@ y0 = val;
 yp0 = f([], y0);
 
 % Compute consistent initial conditions.
+tic
 [y01,yp01] = decic(@Fimpl,tspan(1),y0,[],yp0,[],opts);
 
-tic
+
 sol = ode15i(@Fimpl,tspan,y01,yp01,opts);
 toc
 
 yend = sol.y(:, 11);
+% Norm der Quaternionen
+Q = norm(yend(4:7))
+% Differenz zur 1
+if abs(Q - 1) < 1e-4 % in der Toleranz von 1e-5
+    disp('Yes');
+end
 end
 
 function res = Fimpl(t,y,yp)
@@ -32,6 +39,21 @@ function res = mass(t, y)
      res = diag([ones(3, 1); y(4:7); ones(6, 1)]);
 end
 
+function [dfdy,dfdyp] = Jac(t,y,yp)
+      % Jacobian function for fully implicit Burger's equation
+      J = JacY(y);
+      Jx = J(1:13, 1:13);
+      dfdy = Jx;  % ODE15i will use numerical approximation.
+      dfdyp = mass(t,y);
+end
+
+
+function [dfdy] = JacY(y)
+robot = Robot();
+[r, q,v,omega,u_,Iges,IM,m,kT,kQ,d,g] = getParams(y, robot);
+dfdy = [0 0 0 -2 * v(2) * q(4) + 2 * v(3) * q(3) 2 * v(2) * q(3) + 2 * v(3) * q(4) -4 * v(1) * q(3) + 2 * v(2) * q(2) + 2 * v(3) * q(1) -4 * v(1) * q(4) - 2 * v(2) * q(1) + 2 * v(3) * q(2) 1 - 2 * q(3) ^ 2 - 2 * q(4) ^ 2 -2 * q(1) * q(4) + 2 * q(2) * q(3) 2 * q(1) * q(3) + 2 * q(2) * q(4) 0 0 0 0 0 0 0; 0 0 0 2 * v(1) * q(4) - 2 * v(3) * q(2) 2 * v(1) * q(3) - 4 * v(2) * q(2) - 2 * v(3) * q(1) 2 * v(1) * q(2) + 2 * v(3) * q(4) 2 * v(1) * q(1) - 4 * v(2) * q(4) + 2 * v(3) * q(3) 2 * q(1) * q(4) + 2 * q(2) * q(3) 1 - 2 * q(2) ^ 2 - 2 * q(4) ^ 2 -2 * q(1) * q(2) + 2 * q(3) * q(4) 0 0 0 0 0 0 0; 0 0 0 -2 * v(1) * q(3) + 2 * v(2) * q(2) 2 * v(1) * q(4) + 2 * v(2) * q(1) - 4 * v(3) * q(2) -2 * v(1) * q(1) + 2 * v(2) * q(4) - 4 * v(3) * q(3) 2 * v(1) * q(2) + 2 * v(2) * q(3) -2 * q(1) * q(3) + 2 * q(2) * q(4) 2 * q(1) * q(2) + 2 * q(3) * q(4) 1 - 2 * q(2) ^ 2 - 2 * q(3) ^ 2 0 0 0 0 0 0 0; 0 0 0 -(q(2) * omega(1)) / 0.2e1 - (q(3) * omega(2)) / 0.2e1 - (q(4) * omega(3)) / 0.2e1 -(q(1) * omega(1)) / 0.2e1 -(q(1) * omega(2)) / 0.2e1 -(q(1) * omega(3)) / 0.2e1 0 0 0 -(q(1) * q(2)) / 0.2e1 -(q(1) * q(3)) / 0.2e1 -(q(1) * q(4)) / 0.2e1 0 0 0 0; 0 0 0 (q(2) * omega(1)) / 0.2e1 (q(1) * omega(1)) / 0.2e1 - (q(4) * omega(2)) / 0.2e1 + (q(3) * omega(3)) / 0.2e1 (q(2) * omega(3)) / 0.2e1 -(q(2) * omega(2)) / 0.2e1 0 0 0 (q(1) * q(2)) / 0.2e1 -(q(2) * q(4)) / 0.2e1 (q(2) * q(3)) / 0.2e1 0 0 0 0; 0 0 0 (q(3) * omega(2)) / 0.2e1 -(q(3) * omega(3)) / 0.2e1 (q(4) * omega(1)) / 0.2e1 + (q(1) * omega(2)) / 0.2e1 - (q(2) * omega(3)) / 0.2e1 (q(3) * omega(1)) / 0.2e1 0 0 0 (q(3) * q(4)) / 0.2e1 (q(1) * q(3)) / 0.2e1 -(q(2) * q(3)) / 0.2e1 0 0 0 0; 0 0 0 (q(4) * omega(3)) / 0.2e1 (q(4) * omega(2)) / 0.2e1 -(q(4) * omega(1)) / 0.2e1 -(q(3) * omega(1)) / 0.2e1 + (q(2) * omega(2)) / 0.2e1 + (q(1) * omega(3)) / 0.2e1 0 0 0 -(q(3) * q(4)) / 0.2e1 (q(2) * q(4)) / 0.2e1 (q(1) * q(4)) / 0.2e1 0 0 0 0; 0 0 0 2 * g * q(3) -2 * g * q(4) 2 * g * q(1) -2 * g * q(2) 0 omega(3) -omega(2) 0 -v(3) v(2) 0 0 0 0; 0 0 0 -2 * g * q(2) -2 * g * q(1) -2 * g * q(4) -2 * g * q(3) -omega(3) 0 omega(1) v(3) 0 -v(1) 0 0 0 0; 0 0 0 0 4 * g * q(2) 4 * g * q(3) 0 omega(2) -omega(1) 0 -v(2) v(1) 0 2 * kT * u_(1) / m 2 * kT * u_(2) / m 2 * kT * u_(3) / m 2 * kT * u_(4) / m; 0 0 0 0 0 0 0 0 0 0 0 -(IM * u_(1) - IM * u_(2) + IM * u_(3) - IM * u_(4) + Iges(3) * omega(3) - omega(3) * Iges(2)) / Iges(1) omega(2) * (-Iges(3) + Iges(2)) / Iges(1) -IM * omega(2) / Iges(1) (2 * d * kT * u_(2) + IM * omega(2)) / Iges(1) -IM * omega(2) / Iges(1) (-2 * d * kT * u_(4) + IM * omega(2)) / Iges(1); 0 0 0 0 0 0 0 0 0 0 (IM * u_(1) - IM * u_(2) + IM * u_(3) - IM * u_(4) - omega(3) * Iges(1) + Iges(3) * omega(3)) / Iges(2) 0 -omega(1) * (Iges(1) - Iges(3)) / Iges(2) (-2 * d * kT * u_(1) + IM * omega(1)) / Iges(2) -IM * omega(1) / Iges(2) (2 * d * kT * u_(3) + IM * omega(1)) / Iges(2) -IM * omega(1) / Iges(2); 0 0 0 0 0 0 0 0 0 0 omega(2) * (-Iges(2) + Iges(1)) / Iges(3) omega(1) * (-Iges(2) + Iges(1)) / Iges(3) 0 -2 * kQ * u_(1) / Iges(3) 2 * kQ * u_(2) / Iges(3) -2 * kQ * u_(3) / Iges(3) 2 * kQ * u_(4) / Iges(3);];
+
+end
 
 function res= f(t, val)
 
